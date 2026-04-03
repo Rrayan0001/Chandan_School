2a3ca619ad98c58886260f1391c582e0a90e2e24
School Chandan <schoolchandan@gmail.com>
2026-04-03 15:25:40 +0530

    feat: remove welcome popup and simplified entry flow

--- components/WelcomeFlow.tsx
+++ components/WelcomeFlow.tsx
@@ -2,15 +2,15 @@
 
 import { useState } from "react";
 import { WelcomeAnimation } from "./WelcomeAnimation";
-import { WelcomePopup } from "./WelcomePopup";
 
 export function WelcomeFlow() {
   const [animationDone, setAnimationDone] = useState(false);
 
   return (
     <>
-      <WelcomeAnimation onFinished={() => setAnimationDone(true)} />
-      <WelcomePopup isVisible={animationDone} />
+      {!animationDone && (
+        <WelcomeAnimation onFinished={() => setAnimationDone(true)} />
+      )}
     </>
   );
 }
